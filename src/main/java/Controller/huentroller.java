51c0b65e57652b85cf459859a626376421d92d37
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-19 16:52:45 +0200

    First Commit

---
+++ src/main/java/Controller/huentroller.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class huentroller {
+    @GetMapping("/mawaru")
+    String huentroller1(){
+        System.out.println("Was called!");
+        return "huindex";
+    }
+}
