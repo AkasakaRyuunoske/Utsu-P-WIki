90c7214d18d3e1b91457f98cc34a9ae41fe136aa
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-19 14:36:16 +0200

    First Commit

--- src/main/java/UtsuPWiki/UtsuP/UtsuPApplication.java
+++ src/main/java/UtsuPWiki/UtsuP/UtsuPApplication.java
@@ -1,12 +1,22 @@
 package UtsuPWiki.UtsuP;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class UtsuPApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UtsuPApplication.class, args);
+		log.info("Application Started!");
+		log.info("\n" +
+				" Воля может и должна быть предметом гордости гораздо больше, нежели талант.\n" +
+				" Если талант — это развитие природных склонностей, \n" +
+				" то твёрдая воля — это ежеминутно одерживаемая победа над инстинктами,\n" +
+				" над влечениями, которые воля обуздывает и подавляет, над препятствиями\n" +
+				" и преградами, которые она осиливает, над всяческими трудностями,\n" +
+				" которые она героически преодолевает. \" (Оноре де Бальзак)");
 	}
 }
