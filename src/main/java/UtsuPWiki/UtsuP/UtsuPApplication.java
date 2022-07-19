e580c2b7afd32981170ff38466ab49366bc74ef2
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-19 14:07:03 +0200

    First Commit

---
+++ src/main/java/UtsuPWiki/UtsuP/UtsuPApplication.java
@@ -0,0 +1,12 @@
+package UtsuPWiki.UtsuP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsuPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsuPApplication.class, args);
+	}
+}
