2c43ede7345e96c1677d2287a5f9b89713b5cf3d
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-19 16:53:08 +0200

    First Commit

--- src/main/java/Config/WebSecurityConfig.java
+++ src/main/java/Config/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package Config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfiguration {
+@Log4j2
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
@@ -25,14 +26,15 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
 //
 //        return provider;
 //    }
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception{
-//        httpSecurity
-//                .csrf()
-//                .disable()
-//                .authorizeRequests()
-//                .antMatchers("/")
-//                .permitAll()
-//                .and();
-//    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        log.info("Was called!");
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .and();
+    }
 }
