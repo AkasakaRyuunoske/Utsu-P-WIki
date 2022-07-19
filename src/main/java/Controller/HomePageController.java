7e8d2280d02c3e9b402591023258c14a3b876963
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-19 15:13:16 +0200

    First Commit

--- src/main/java/Controller/HomePageController.java
+++ src/main/java/Controller/HomePageController.java
@@ -2,14 +2,16 @@ package Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @Controller
 public class HomePageController {
     @GetMapping("/")
-    String indexController(){
+    String indexController(Model model){
         log.info("Was called!");
+        model.addAttribute("message","<h1>Utsu-P  --  The Dying Message</h1>");
         return "index";
     }
 }
