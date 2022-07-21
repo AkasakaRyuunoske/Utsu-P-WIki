eea6ba73d8250098f42bee51f3d897a291068c68
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-21 17:14:10 +0200

    Entity first compilation following E-R model

--- src/main/java/UtsuPWiki/Entity/Delivery.java
+++ src/main/java/UtsuPWiki/Entity/Delivery.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,5 +13,29 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 @Builder
 public class Delivery {
+    @Id
+    @SequenceGenerator(name = "Delivery_Sequence", sequenceName = "Delivery_Sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Delivery_Sequence")
+    @Column(name = "ID")
+    private long id;
 
+    @Column(name = "Address_To", nullable = false)
+    private String addressTo;
+
+    @Column(name = "Address_From", nullable = false)
+    private String addressFrom;
+
+    @Column(name = "Date", nullable = false)
+    private String date;
+
+    @Column(name = "Additional_Info")
+    private String additionalInfo;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_courier_fk", referencedColumnName = "id")
+    private Couriers couriers;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_order_fk", referencedColumnName = "id")
+    private Orders orders;
 }
