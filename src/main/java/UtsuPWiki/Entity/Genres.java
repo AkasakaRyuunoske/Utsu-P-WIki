6ec352d5c87bfab54a3a865bb9274775735d85cf
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-21 17:26:44 +0200

    Entity first compilation following E-R model

--- src/main/java/UtsuPWiki/Entity/Genres.java
+++ src/main/java/UtsuPWiki/Entity/Genres.java
@@ -1,4 +1,27 @@
 package UtsuPWiki.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Genres {
+    @Id
+    @SequenceGenerator(name = "Genres_Sequence", sequenceName = "Genres_Sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Genres_Sequence")
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "Name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
 }
