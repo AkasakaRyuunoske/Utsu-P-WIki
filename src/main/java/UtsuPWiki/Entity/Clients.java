6ef1be14ad41d6fbc4abf8b1d072c0f5192c5a27
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-21 17:14:01 +0200

    Entity first compilation following E-R model

--- src/main/java/UtsuPWiki/Entity/Clients.java
+++ src/main/java/UtsuPWiki/Entity/Clients.java
@@ -1,9 +1,11 @@
 package UtsuPWiki.Entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.sql.Blob;
 
 @Entity
 @Data
@@ -21,25 +23,25 @@ public class Clients {
     @Column(name = "UserName", nullable = false, unique = true)
     private String userName;
 
-    @Column(name = "Password", nullable = false, unique = false)
+    @Column(name = "Password", nullable = false)
     private String password;
 
     @Column(name = "Email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "Role", nullable = false, unique = false)
+    @Column(name = "Role", nullable = false)
     private String role = "Client";
 
-    @Column(name = "Content_Created", nullable = false, unique = false)
+    @Column(name = "Content_Created", nullable = false)
     private int contentCreated = 0;
 
     //todo idk how to make the thing, so for now it'll be commented out
 //    @Column(name = "Profile_Photo", nullable = false, unique = false)
 //    private Blob profilePhoto;
 
-    @Column(name = "Session_Id", nullable = true, unique = false)
+    @Column(name = "Session_Id")
     private String sessionId;
 
-    @Column(name = "Default_Address", nullable = true, unique = false)
+    @Column(name = "Default_Address")
     private String default_Address = "none";
 }
