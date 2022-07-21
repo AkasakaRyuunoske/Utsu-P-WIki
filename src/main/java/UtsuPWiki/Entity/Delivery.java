0ee21a87379cbb41edffb84dbaa0da3bebd88c80
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-21 16:45:58 +0200

    @ComponentScan problems resolved: @Entities Compiled

--- src/main/java/UtsuPWiki/Entity/Delivery.java
+++ src/main/java/UtsuPWiki/Entity/Delivery.java
@@ -1,4 +1,17 @@
 package UtsuPWiki.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Delivery {
+
 }
