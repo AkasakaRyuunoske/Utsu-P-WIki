4c025c2019fa9ee83cded28e4f334460bea408fe
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-22 15:23:44 +0200

    First Commit

---
+++ src/main/java/UtsuPWiki/Repository/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package UtsuPWiki.Repository;
+
+import UtsuPWiki.Entity.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
