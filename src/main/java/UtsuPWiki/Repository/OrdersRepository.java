ae1e12b5b96922a629497de2363535c08fa868b8
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-22 15:23:59 +0200

    First Commit

---
+++ src/main/java/UtsuPWiki/Repository/OrdersRepository.java
@@ -0,0 +1,9 @@
+package UtsuPWiki.Repository;
+
+import UtsuPWiki.Entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+}
