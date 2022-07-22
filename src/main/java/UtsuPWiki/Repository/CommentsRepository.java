737c92b69fa33c17ce195b2622b7f78d0140e894
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-22 15:23:37 +0200

    First Commit

---
+++ src/main/java/UtsuPWiki/Repository/CommentsRepository.java
@@ -0,0 +1,9 @@
+package UtsuPWiki.Repository;
+
+import UtsuPWiki.Entity.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comments, Long> {
+}
