ced4f17587e3e1601c014fd97fa0b6d64e6c85ca
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-22 15:24:10 +0200

    First Commit

---
+++ src/main/java/UtsuPWiki/Repository/ProductsRepository.java
@@ -0,0 +1,9 @@
+package UtsuPWiki.Repository;
+
+import UtsuPWiki.Entity.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<Products, Long> {
+}
