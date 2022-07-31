e3a1d7d1b5545ca893ae1cc7fff4ebe662ee8efa
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-07-31 21:36:45 +0200

    First Compilation

--- src/main/java/UtsuPWiki/Controller/AuthorMainPageController.java
+++ src/main/java/UtsuPWiki/Controller/AuthorMainPageController.java
@@ -1,4 +1,21 @@
 package UtsuPWiki.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class AuthorMainPageController {
+    @GetMapping("/authors")
+    public String authorsGETController(Model model){
+        model.addAttribute("currentPage","*/authors");
+        return "authors";
+    }
+    @GetMapping("/authors/{author}")
+    public String authorGETController(@PathVariable String author, Model model){
+        model.addAttribute("author","utsu-p");
+        model.addAttribute("currentPage","*/authors/{author}");
+        return "authors";
+    }
 }
