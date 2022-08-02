bd698c8ba94b0f5bb8f323ce33d7e0d18659f650
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-02 16:02:56 +0200

    Minor changes

--- src/main/java/UtsuPWiki/Controller/VocaloidAlbumsController.java
+++ src/main/java/UtsuPWiki/Controller/VocaloidAlbumsController.java
@@ -1,10 +1,12 @@
 package UtsuPWiki.Controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Log4j2
 @Controller
 public class VocaloidAlbumsController {
     @GetMapping("/authors/{author}/albums")
