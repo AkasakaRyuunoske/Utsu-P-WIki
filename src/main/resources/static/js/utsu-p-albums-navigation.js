e09200137b9a8686b59988f4ee2de78c804c675f
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-02 16:03:34 +0200

    First Commit

---
+++ src/main/resources/static/js/utsu-p-albums-navigation.js
@@ -0,0 +1,15 @@
+document.getElementById("Algorithm").onclick = function () {
+	window.location.href = "/authors/Utsu-p/albums/Algorithm";
+}
+
+document.getElementById("Post-Traumatic-Stress-Disorder").onclick = function () {
+	window.location.href = "/authors/Utsu-p/albums/Post-Traumatic-Stress-Disorder";
+}
+
+document.getElementById("Moksha").onclick = function () {
+	window.location.href = "/authors/Utsu-p/albums/Moksha";
+}
+
+document.getElementById("Warufuzake").onclick = function () {
+	window.location.href = "/authors/Utsu-p/albums/Warufuzake";
+}
