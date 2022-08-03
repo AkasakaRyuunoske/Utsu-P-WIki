5c802c53cea02235f8708b66263b25743f149110
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-03 19:47:22 +0200

    Navigation Improvement

--- src/main/java/UtsuPWiki/Controller/HomePageController.java
+++ src/main/java/UtsuPWiki/Controller/HomePageController.java
@@ -1,6 +1,8 @@
 package UtsuPWiki.Controller;
 
+import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Log4j2
 @Controller
 public class HomePageController {
+    @Autowired
+    Navigation navigation;
+
     @GetMapping("/")
     String indexController(Model model){
         log.info("indexController Was called!");
 
-        model.addAttribute("homePage","home/");
-        model.addAttribute("authorsPage","authors/");
+        navigation.addLocations(model, "home/sanman/de/fuantei/na/kono");
 
         return "index";
     }
