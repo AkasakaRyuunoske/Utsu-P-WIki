f983c14183f1cfc954713e209bc8c069be3fc3e4
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-03 19:47:44 +0200

    Navigation Improvement

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -5,27 +5,6 @@ document.getElementById("currentUser").onclick = function(){
 	window.location.href = "/logout";
 }
 
-document.getElementById("homePage").onclick = function(){
-	window.location.href = "/";
-}
-
-document.getElementById("authorsPage").onclick = function(){
-	window.location.href = "/authors/";
-}
-	//todo
-// document.getElementById("authorPage").onclick = function(){
-// 	window.location.href = "/authors/";
-// }
-document.getElementById("albumsPage").onclick = function(){
-	window.location.href = "/albums/";
-}
-	//todo
-// document.getElementById("albumPage").onclick = function(){
-// 	window.location.href = "/albums/";
-// }
-
-
-
 document.getElementById("Utsu-P").onclick = function () {
 
 	if (counter == 0) {
@@ -39,18 +18,10 @@ document.getElementById("Utsu-P").onclick = function () {
 	}
 }
 
-document.getElementById("Authors").onclick = function () {
-	window.location.href = "/authors/";
-}
+function navigate(element) {
+	element.id = "location";
 
-document.getElementById("Community").onclick = function () {
-	window.location.href = "/community/";
-}
+	let location = document.getElementById('location').innerHTML.toLowerCase();
 
-document.getElementById("Create").onclick = function () {
-	window.location.href = "/create/";
-}
-
-document.getElementById("Buy").onclick = function () {
-	window.location.href = "/buy/";
+	window.location.href = location;
 }
