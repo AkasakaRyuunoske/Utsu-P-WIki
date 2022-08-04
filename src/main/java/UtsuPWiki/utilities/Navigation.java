aa4dd9c0046a685e4f9fb97cd62520a85c8a1a9b
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-04 14:39:19 +0200

    Improving navigation

--- src/main/java/UtsuPWiki/utilities/Navigation.java
+++ src/main/java/UtsuPWiki/utilities/Navigation.java
@@ -5,22 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Log4j2
 @Component
 public class Navigation {
 
-    public void addLocations(Model model, String location){
-        ArrayList<String> newLocation = new ArrayList<>();
-
+    public static void addLocations(Model model, String location){
         String[] newLocation1 = location.split("(?<=/)");
-        for (int i = 0; newLocation1.length > i; i++) {
-            log.info("Split #" + i + " result : " + newLocation1[i]);
-            newLocation.add(newLocation1[i]);
-        }
 
-        log.info("newLocation : " + newLocation);
-        log.info("location " + location);
+        ArrayList<String> newLocation = new ArrayList<>(Arrays.asList(newLocation1));
+
         model.addAttribute("location", newLocation);
     }
 }
