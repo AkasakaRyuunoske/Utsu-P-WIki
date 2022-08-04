3cd4c097e3a0fc90961e51ff7aa52626c8a80706
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-04 20:28:57 +0200

    Navigation improved

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -19,15 +19,43 @@ document.getElementById("Utsu-P").onclick = function () {
 }
 
 function navigate(element) {
-	element.id = "location";
+	element.id 	 	   = "location";
+
+	let PREFIX_LOCAL   = "http://localhost:8080/";
+	let PREFIX_HEROKU  = "https://.herokuapp.com/";
 
 	let location = document.getElementById('location').innerHTML.toLowerCase();
-	
+	let locationsContainer = document.getElementById('locationsContainer').innerHTML.toLowerCase();
+	let locationIndex = locationsContainer.indexOf(location);
+	let sliceTo;
+	if (locationIndex == -1) {
+		sliceTo = locationIndex + (location.length + 1);
+	} else {
+		sliceTo = locationIndex + location.length;
+	}
+
+	console.log(location + " starting location is that");
+	console.log(locationsContainer + " locationsContainer is that");
+	console.log(locationIndex + " locationIndex is that");
+	console.log(sliceTo + " sliceTo is that");
+
+	location = location.slice(0, sliceTo);
+
+	console.log(location + " location after optimisation");
 	if (location == "home/") {
-		window.location.href = "/";
+
+		window.location.href = PREFIX_LOCAL;
 
 	} else {
 
-		window.location.href = location;
+		window.location.href = PREFIX_LOCAL + location;
+
 	}
 }
+
+// let toSearch = "/utsu-p";
+// let text = "/authors/utsu-p/albums/Warufuzake";
+// let result = text.indexOf(toSearch);
+// let howToSlice = result + toSearch.length;
+// let finalString = text.slice(0,15)
+// document.getElementById("demo").innerHTML = finalString; 
