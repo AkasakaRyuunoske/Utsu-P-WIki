9b260c9a6afbedfb859bd8f4f19b7df8097fbe14
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-05 16:22:59 +0200

    Registration Added

--- src/main/java/UtsuPWiki/Repository/ClientsRepository.java
+++ src/main/java/UtsuPWiki/Repository/ClientsRepository.java
@@ -3,6 +3,7 @@ package UtsuPWiki.Repository;
 import UtsuPWiki.Entity.Clients;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClientsRepository extends JpaRepository<Clients, Long> {
