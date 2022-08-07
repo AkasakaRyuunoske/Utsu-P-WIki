46be52b349344ca8fc38c0d8ae3cb7a6a10c6176
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-07 19:51:49 +0200

    Authentication through JWT half way done

--- src/main/java/UtsuPWiki/Filter/JWTAuthenticationFilter.java
+++ src/main/java/UtsuPWiki/Filter/JWTAuthenticationFilter.java
@@ -6,27 +6,31 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @Log4j2
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(11);
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -35,10 +39,16 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         try {
             Clients creds = new ObjectMapper()
                     .readValue(request.getInputStream(), Clients.class);
+
+            log.info(creds.getUserName() + " creds user name ");
+            log.info(creds.getPassword() + " creds password ");
+            log.info(passwordEncoder + " password encoder is he");
+            log.info(passwordEncoder.encode(creds.getPassword()) + " creds password encoded");
+
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             creds.getUserName(),
-                            creds.getPassword(),
+                            creds.getPassword(),    //Is already using defined as @Bean password encoder(BCrypt(11))
                             new ArrayList<>())
             );
         } catch (IOException e) {
@@ -48,7 +58,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
-        log.info("Was called!");
+        log.info("Was called! successfulAuthentication");
         String token = JWT.create()
                 .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
