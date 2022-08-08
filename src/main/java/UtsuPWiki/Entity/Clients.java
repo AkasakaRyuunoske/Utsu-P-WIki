1ab3ed736644d48d35d6ef2d71a208db2a9aebf3
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-08 19:07:50 +0200

    JWT Authentication almost done

--- src/main/java/UtsuPWiki/Entity/Clients.java
+++ src/main/java/UtsuPWiki/Entity/Clients.java
@@ -1,5 +1,6 @@
 package UtsuPWiki.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Clients {
 
     @Id
