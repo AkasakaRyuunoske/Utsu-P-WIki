42999b61031a0b23efc79e40587fa44b3abbd4db
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-08 19:08:33 +0200

    JWT Authentication almost done

--- src/main/java/UtsuPWiki/Controller/LoginController.java
+++ src/main/java/UtsuPWiki/Controller/LoginController.java
@@ -1,6 +1,7 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.Entity.Clients;
+import UtsuPWiki.utilities.SecurityConstants;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Log4j2
 @Controller
@@ -25,6 +28,6 @@ public class LoginController {
         log.info(client.getPassword() + " password");
         log.info(client.getUserName() + " username");
 
-        return "index";
+        return "login";
     }
 }
