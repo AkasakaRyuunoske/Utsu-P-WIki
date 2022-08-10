6b1baf829d4c2570f4a86ae7bad54d55b6f8264f
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-10 19:11:02 +0200

    JWT adaptation

--- src/main/java/UtsuPWiki/Controller/LoginController.java
+++ src/main/java/UtsuPWiki/Controller/LoginController.java
@@ -1,24 +1,16 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.Entity.Clients;
-import UtsuPWiki.Repository.ClientsRepository;
-import UtsuPWiki.utilities.SecurityConstants;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletResponse;
-
 @Log4j2
 @Controller
 public class LoginController {
-    @Autowired
-    ClientsRepository clientsRepository;
-
     @PostMapping("/login")
     public String GETLogin(Model model){
         model.addAttribute("client", new Clients());
