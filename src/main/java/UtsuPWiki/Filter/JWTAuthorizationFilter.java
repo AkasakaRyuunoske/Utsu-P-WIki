bc15bfc03cb2148c6e8682df7ad43a97fb294c27
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-11 15:07:09 +0200

    JWT finally working

--- src/main/java/UtsuPWiki/Filter/JWTAuthorizationFilter.java
+++ src/main/java/UtsuPWiki/Filter/JWTAuthorizationFilter.java
@@ -8,28 +8,33 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * Poorly made both JWTAuthorization and JWTAuthentication filters.
  *
- *  Every thing was made following <a href="https://javatodev.com/spring-boot-jwt-authentication/">this guide.</a>
+ *  Every thing was made following this guide. https://javatodev.com/spring-boot-jwt-authentication/
  *
  * */
 
 @WebFilter("/*")
 @Log4j2
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public String token;
+
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                               HttpServletResponse response,
@@ -53,7 +58,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request,
                                                                   HttpServletResponse response) {
-        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+        token = request.getHeader(SecurityConstants.HEADER_STRING);
         if (token != null) {
 
             String user = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()))
@@ -61,7 +66,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
                     .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""))
                     .getSubject();
 
-            response.setHeader(SecurityConstants.HEADER_STRING, token);
+            response.addHeader(SecurityConstants.HEADER_STRING, token);
             if (user != null) {
                 return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
             }
