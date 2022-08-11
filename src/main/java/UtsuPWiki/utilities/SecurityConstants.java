f71b557bddb58038197429f035cd2587f6180a3d
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-11 15:07:13 +0200

    JWT finally working

--- src/main/java/UtsuPWiki/utilities/SecurityConstants.java
+++ src/main/java/UtsuPWiki/utilities/SecurityConstants.java
@@ -4,7 +4,7 @@ package UtsuPWiki.utilities;
 public class SecurityConstants {
 
     public static final String SECRET = "sanmandefuanteinakonosekaikakumeihookoshiteyoaigaseikaininare";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 3600_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/login";
