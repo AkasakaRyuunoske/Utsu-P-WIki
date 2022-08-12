42c8915252c490b2aa135990a2947701da9b6228
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-12 16:25:46 +0200

    Unused field deleted

--- src/main/java/UtsuPWiki/utilities/SecurityConstants.java
+++ src/main/java/UtsuPWiki/utilities/SecurityConstants.java
@@ -4,8 +4,7 @@ package UtsuPWiki.utilities;
 public class SecurityConstants {
 
     public static final String SECRET = "sanmandefuanteinakonosekaikakumeihookoshiteyoaigaseikaininare";
-    public static final long EXPIRATION_TIME = 3600_000;
+    public static final long EXPIRATION_TIME = 6000_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/login";
 }
