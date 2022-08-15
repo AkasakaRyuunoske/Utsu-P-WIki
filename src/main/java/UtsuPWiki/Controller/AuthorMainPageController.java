cc0254211f8af193b23e7874b4e0ce7281ce1d4f
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-15 20:08:51 +0200

    JWT adaptation in progress

--- src/main/java/UtsuPWiki/Controller/AuthorMainPageController.java
+++ src/main/java/UtsuPWiki/Controller/AuthorMainPageController.java
@@ -1,16 +1,20 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.utilities.Navigation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+@Log4j2
 @Controller
 public class AuthorMainPageController {
     @GetMapping("/authors")
     public String authorsGETController(Model model){
         Navigation.addLocations(model, "home/authors");
+        log.info("Was called!");
 
         return "authors";
     }
