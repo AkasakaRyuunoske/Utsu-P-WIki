2c9fb1068362c01a7136da50895fef213458aec7
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-15 20:09:19 +0200

    JWT adaptation in progress

--- src/main/java/UtsuPWiki/ApiController/TheDyingMessage.java
+++ src/main/java/UtsuPWiki/ApiController/TheDyingMessage.java
@@ -3,6 +3,7 @@ package UtsuPWiki.ApiController;
 import UtsuPWiki.utilities.SecurityConstants;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,18 +17,19 @@ import java.io.IOException;
 @Controller
 public class TheDyingMessage {
 
-    @PostMapping("/test-call")
-    public String   theDyingMessageReceiver(HttpServletRequest request, HttpServletResponse response){
-        String location = request.getHeader("Location");
+    @GetMapping("/test-call")
+    public String theDyingMessageReceiver(HttpServletRequest request, HttpServletResponse response){
 
         log.info("custom header: " + request.getHeader("user"));
         log.info("authorization filter is: "  + request.getHeader(SecurityConstants.HEADER_STRING));
-        log.info("Location from Front-End is: " + location);
-
-//        response.setHeader("Location", "http://localhost:8080/authors");
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+        log.info("token is: " + token);
+        response.setHeader(SecurityConstants.HEADER_STRING, token);
+//        response.setHeader("The Dying Message", "By Utsu-P");
+//        response.setHeader("Location", "/authors");
 //        response.setStatus(302);
 //        try {
-//            response.sendRedirect("http://localhost:8080/authors");
+//            response.sendRedirect("/authors");
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
