9b7cc6cab9cc10cb6c00f4cd11d0c801a0bfa110
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-16 20:03:20 +0200

    Adding error handling

--- src/main/java/UtsuPWiki/Error/CustomErrorHandler.java
+++ src/main/java/UtsuPWiki/Error/CustomErrorHandler.java
@@ -1,10 +1,14 @@
 package UtsuPWiki.Error;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +19,11 @@ public class CustomErrorHandler implements ErrorController {
     private final String ERROR_PAGE = "error"; // Name of the view that will be returned
     private final String ERROR_MESSAGE = "errorMessage";
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
+    @ExceptionHandler(value = { IllegalArgumentException.class,
+            IllegalStateException.class, ExpiredJwtException.class,
+            JWTVerificationException.class,
+            TokenExpiredException.class})
     public String handleErrors(HttpServletRequest httpServletRequest,
                                  HttpServletResponse httpServletResponse,
                                Model model){
@@ -51,6 +59,12 @@ public class CustomErrorHandler implements ErrorController {
             case 405:
                 model.addAttribute(ERROR_MESSAGE,
                         "HTTP method not allowed.");
+                break;
+
+
+            case 453:
+                model.addAttribute(ERROR_MESSAGE,
+                        "Custom Error.");
                 break;
 
             case 500:
