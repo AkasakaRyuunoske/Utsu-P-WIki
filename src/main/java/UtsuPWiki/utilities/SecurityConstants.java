8d665fe2198adaeeb75f7bc9426fb1f6f28bd4c3
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-16 17:15:13 +0200

    JWT in cookie finally working implementation

--- src/main/java/UtsuPWiki/utilities/SecurityConstants.java
+++ src/main/java/UtsuPWiki/utilities/SecurityConstants.java
@@ -5,6 +5,6 @@ public class SecurityConstants {
 
     public static final String SECRET = "sanmandefuanteinakonosekaikakumeihookoshiteyoaigaseikaininare";
     public static final long EXPIRATION_TIME = 6000_000;
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_PREFIX = "Bearer_";
     public static final String HEADER_STRING = "Authorization";
 }
