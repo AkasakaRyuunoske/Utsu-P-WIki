4ca428d8d39ccdff4fb2452c290d704050458c1d
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-17 16:06:39 +0200

    New ShouldNotFilter paths added. Token is not null condition added

--- src/main/java/UtsuPWiki/Filter/JWTAuthorizationFilter.java
+++ src/main/java/UtsuPWiki/Filter/JWTAuthorizationFilter.java
@@ -3,12 +3,10 @@ package UtsuPWiki.Filter;
 import UtsuPWiki.utilities.SecurityConstants;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import io.jsonwebtoken.JwtException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
@@ -61,22 +59,22 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         try {
             Cookie cookie = WebUtils.getCookie(request, "JWT");
 
-            try {
+            if (cookie != null) {
                 token = cookie.getValue();
-            }catch (NullPointerException nullPointerException){
-                throw new NullPointerException("Right token is not found.");
+            } else {
+                throw new NullPointerException("The token is missing in the request.");
             }
+
 
             String user = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()))
                     .build()
                     .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""))
                     .getSubject();
 
-            response.addHeader(SecurityConstants.HEADER_STRING, token);
-
             return new UsernamePasswordAuthenticationToken(user, "none", new ArrayList<>());
 
         } catch (JwtException jwtException){
+            response.setStatus(500);
             throw new IllegalStateException("Token cannot be trusted or is expired. Error message: " + jwtException);
         }
     }
@@ -85,11 +83,14 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request){
         String path = request.getRequestURL().toString();
 
-        return path.contains("/css/") ||
-                path.contains("/js/") ||
-                path.contains(".jpg") ||
-                path.contains(".png") ||
-                path.contains("/test-call") ||
+        return path.contains("/css/")           ||
+                path.contains("/js/")           ||
+                path.contains(".jpg")           ||
+                path.contains(".png")           ||
+                path.contains("/test-call")     ||
+                path.contains("/login")         ||
+                path.contains("/registration")  ||
+                path.contains("/logout")        ||
                 path.contains("/error");
     }
 
@@ -101,10 +102,5 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilterErrorDispatch() {
         return true;
-    }
-
-    @ExceptionHandler({TokenExpiredException.class })
-    public void handleException() {
-        log.info("The error is handled");
     }
 }
