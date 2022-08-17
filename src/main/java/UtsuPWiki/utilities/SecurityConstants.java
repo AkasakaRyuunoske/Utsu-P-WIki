007fedc790b8cbe70cf37ef0707151779e67020e
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-17 16:01:20 +0200

    Expiration time changed to 3600_000(one hour)

--- src/main/java/UtsuPWiki/utilities/SecurityConstants.java
+++ src/main/java/UtsuPWiki/utilities/SecurityConstants.java
@@ -2,9 +2,40 @@ package UtsuPWiki.utilities;
 
 
 public class SecurityConstants {
+    //one hour
+    public static final long EXPIRATION_TIME = 3600_000;
 
-    public static final String SECRET = "sanmandefuanteinakonosekaikakumeihookoshiteyoaigaseikaininare";
-    public static final long EXPIRATION_TIME = 6000_000;
+    //Delimiter of the token
     public static final String TOKEN_PREFIX = "Bearer_";
+
+    //Does not have much of use but might be useful later
     public static final String HEADER_STRING = "Authorization";
+
+    public static final String SECRET =
+            "sanman_de_fuantei_na_kono_sekai"       +
+            "kakumei_ho_okoshite_yo"                +
+            "_"                                     +
+            "'AI'_ga_seikai_ni_nare"                +
+            "_"                                     +
+            "fuantei_nantei_kankan_ni_kiechate"     +
+            "hakumei_no_hi_terasu_sono_mukou_he"    +
+            "_"                                     +
+            "kiroku_mo_kioku_mo_nai_hito_maishou"   +
+            "kibuo_mo_mirai_mo_nai_kuni_gaishou"    +
+            "_"                                     +
+            "nagai_yoru_wo_oe_asayake_wo_mita"      +
+            "akaku_somaru_senaka_wooo"              +
+            "_"                                     +
+            "NEE_ANATA_WA_ITSUKA_INAKU_NARI_MASU_KA?" +
+            "OWARI_KAKE_NO_KONO_SEKAI_DE"           +
+            "YAOKE_MAE_NO_UREI_TAEGATAI_KODOKU"     +
+            "AWAI_OMOI_TO_SEKIBAKU_WO_DAITA"        +
+            "_"                                     +
+            "senmen_na_dangan_no_kioku_ni"          +
+            "dare_mo_ga_hi_yotte_wa"                +
+            "_"                                     +
+            "DAIPO_GA_DE_NAI"                       +
+            "_"                                     +
+            "anzen_wa_kankan_ni_kiechate"           +
+            "akukeyou_oyoboshita_boku_no_kokoro_he";
 }
