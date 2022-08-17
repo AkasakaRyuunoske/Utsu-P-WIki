53b69a7622f16f4ffad9c1196579a7c81dffd645
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-17 16:00:45 +0200

    Deleted SpringWebMvc

--- src/main/java/UtsuPWiki/UtsuPApplication.java
+++ src/main/java/UtsuPWiki/UtsuPApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Log4j2
 @SpringBootApplication
-@EnableWebMvc
 public class UtsuPApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UtsuPApplication.class, args);
