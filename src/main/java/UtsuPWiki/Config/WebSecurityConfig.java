7630d585cf080410990cd5307143972ddff8d5b5
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-18 20:23:12 +0200

    Improving exception handling

--- src/main/java/UtsuPWiki/Config/WebSecurityConfig.java
+++ src/main/java/UtsuPWiki/Config/WebSecurityConfig.java
@@ -1,19 +1,34 @@
 package UtsuPWiki.Config;
 
+import UtsuPWiki.Entity.Clients;
 import UtsuPWiki.Filter.JWTAuthenticationFilter;
 import UtsuPWiki.Filter.JWTAuthorizationFilter;
-import UtsuPWiki.Service.AuthenticationUserDetailService;
+import UtsuPWiki.Repository.ClientsRepository;
+import UtsuPWiki.Service.ClientsDetailsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.SpringSecurityMessageSource;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Log4j2
 @Configuration
@@ -24,7 +39,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(11);
     }
     @Autowired
-    public AuthenticationUserDetailService authenticationUserDetailService;
+    public ClientsDetailsService clientsDetailsService;
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(clientsDetailsService);
+        daoAuthenticationProvider.setHideUserNotFoundExceptions(false) ;
+        return daoAuthenticationProvider;
+    }
 
     public final String[] INACCESSIBLE_URLS_BY_NOT_AUTHENTICATED_USERS = {
             "/authors",
@@ -60,8 +82,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(authenticationUserDetailService)
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(clientsDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder(11));
     }
 }
+
+
+
+
+
