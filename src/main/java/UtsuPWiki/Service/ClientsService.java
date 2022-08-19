74b753546aadf042715d755092bd248d7df3daff
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 21:44:31 +0200

    New method added

--- src/main/java/UtsuPWiki/Service/ClientsService.java
+++ src/main/java/UtsuPWiki/Service/ClientsService.java
@@ -1,7 +1,9 @@
 package UtsuPWiki.Service;
 
+import UtsuPWiki.Entity.Clients;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ClientsService {
+    void registerClient(Clients client);
 }
