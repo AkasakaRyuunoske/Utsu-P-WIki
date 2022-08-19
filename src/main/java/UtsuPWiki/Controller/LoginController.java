93e1920cee60c457f352b7a72c73f030b0379d81
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 20:56:38 +0200

    Navigation method improved

--- src/main/java/UtsuPWiki/Controller/LoginController.java
+++ src/main/java/UtsuPWiki/Controller/LoginController.java
@@ -1,14 +1,15 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.Entity.Clients;
+import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Log4j2
@@ -17,7 +18,13 @@ public class LoginController {
 
     //The page client can see and insert data
     @GetMapping("/login")
-    public String GETLoginOnRedirect(@ModelAttribute Clients client, HttpServletResponse response){
+    public String GETLoginOnRedirect(@ModelAttribute Clients client,
+                                     HttpServletResponse response,
+                                     HttpServletRequest request,
+                                     Model model){
+
+        Navigation.addLocations(model, request);
+
         return "login";
     }
 
@@ -26,6 +33,5 @@ public class LoginController {
     @PostMapping("/login")
     public void POSTLogin(Model model){
         model.addAttribute("client", new Clients());
-        log.info("Post Login was called!");
     }
 }
