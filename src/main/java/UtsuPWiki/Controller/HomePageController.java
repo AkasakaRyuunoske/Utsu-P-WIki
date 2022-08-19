e81fadfb0b8050b48822e2cf0d5d7c268baf2650
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 15:41:45 +0200

    Navigation method improved

--- src/main/java/UtsuPWiki/Controller/HomePageController.java
+++ src/main/java/UtsuPWiki/Controller/HomePageController.java
@@ -6,14 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Log4j2
 @Controller
 public class HomePageController {
     @GetMapping("/")
-    String indexController(Model model){
+    String indexController(Model model, HttpServletRequest request){
         log.info("indexController Was called!");
 
-        Navigation.addLocations(model, "home/");
+        Navigation.addLocations(model, request);
 
         return "index";
     }
