200bfa667eb542f68a1d4bb52027056817bc3c04
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 15:42:19 +0200

    Navigation method improved

--- src/main/java/UtsuPWiki/utilities/Navigation.java
+++ src/main/java/UtsuPWiki/utilities/Navigation.java
@@ -4,18 +4,27 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @Log4j2
 @Component
 public class Navigation {
+    public static void addLocations(Model model, HttpServletRequest request){
+        //extract host, in locale it will be => "localhost:8080"
+        String host = request.getHeader("Host" );
 
-    public static void addLocations(Model model, String location){
-        String[] newLocation1 = location.split("(?<=/)");
+        //every request contains it's referer from where protocol is determined (http:// or https://)
+        //this part will be extracted and take pos [0]
+        String[] protocol = request.getHeader("Referer").split("(?<=://)");
 
-        ArrayList<String> newLocation = new ArrayList<>(Arrays.asList(newLocation1));
+        //then useless part is replaced by "home" which is root page
+        String locations_to_split = request.getRequestURL().toString().replace(protocol[0] + host,"home");
+        String[] split_currentLocation = locations_to_split.split("(?<=/)");
 
-        model.addAttribute("location", newLocation);
+        ArrayList<String> currentLocation = new ArrayList<>(Arrays.asList(split_currentLocation));
+
+        model.addAttribute("location", currentLocation);
     }
 }
