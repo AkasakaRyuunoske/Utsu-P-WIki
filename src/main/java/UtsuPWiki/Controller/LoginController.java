01954899c22c20655dba0028773ad6237718cdfa
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 15:41:54 +0200

    Navigation method improved

--- src/main/java/UtsuPWiki/Controller/LoginController.java
+++ src/main/java/UtsuPWiki/Controller/LoginController.java
@@ -1,7 +1,6 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.Entity.Clients;
-import UtsuPWiki.Service.ClientsDetailsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
