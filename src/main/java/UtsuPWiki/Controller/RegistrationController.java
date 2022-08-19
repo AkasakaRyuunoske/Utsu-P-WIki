194223014c8880942ac82f30eba21fb59db60910
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 21:45:52 +0200

    Improved postMapping logic

--- src/main/java/UtsuPWiki/Controller/RegistrationController.java
+++ src/main/java/UtsuPWiki/Controller/RegistrationController.java
@@ -1,11 +1,10 @@
 package UtsuPWiki.Controller;
 
 import UtsuPWiki.Entity.Clients;
-import UtsuPWiki.Repository.ClientsRepository;
+import UtsuPWiki.ServiceImplementations.ClientsServiceImp;
 import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +12,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 
 @Controller
 @Log4j2
 public class RegistrationController {
     @Autowired
-    ClientsRepository clientsRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    ClientsServiceImp clientsServiceImp;
 
     @GetMapping("/registration")
     public String GETRegistration(@ModelAttribute Clients client,
@@ -34,19 +29,16 @@ public class RegistrationController {
         return "registration";
     }
 
-    @PostMapping("/registration-complete")
+    @PostMapping("/registration")
     public String POSTRegistrationComplete(@ModelAttribute Clients client,
                                            Model model,
                                            HttpServletRequest request){
 
         model.addAttribute("client", client);
-
-        client.setRegistrationDate(new Date().toString());
-        client.setPassword(passwordEncoder.encode(client.getPassword()));
-
-        clientsRepository.save(client);
-
         Navigation.addLocations(model, request);
-        return "registration";
+
+        clientsServiceImp.registerClient(client);
+
+        return "redirect:/";
     }
 }
