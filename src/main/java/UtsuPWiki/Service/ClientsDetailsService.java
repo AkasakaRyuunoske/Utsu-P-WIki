db75fcdc30f09e86003bf49d861cd9aa687a11ea
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 21:44:19 +0200

    Documentation added, logs deleted

--- src/main/java/UtsuPWiki/Service/ClientsDetailsService.java
+++ src/main/java/UtsuPWiki/Service/ClientsDetailsService.java
@@ -4,9 +4,7 @@ package UtsuPWiki.Service;
 import UtsuPWiki.Entity.Clients;
 import UtsuPWiki.Error.CustomException;
 import UtsuPWiki.Repository.ClientsRepository;
-
 import lombok.extern.log4j.Log4j2;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,12 +13,17 @@ import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.ArrayList;
 
+
+/**
+ * Custom implementation of UserDetailsService(spring security).
+ *
+ * Exceptions are not much handled here but in JWTAuthenticationFilter.
+ *
+ * */
 
 @Log4j2
 @Service
@@ -31,20 +34,23 @@ public class ClientsDetailsService implements UserDetailsService {
     protected MessageSourceAccessor messages = SpringSecurityMessageSource.getAccessor();
 
     @Override
-    public UserDetails loadUserByUsername(String userName) throws BadCredentialsException, InternalAuthenticationServiceException {
+    public UserDetails loadUserByUsername(String userName)
+            throws BadCredentialsException, InternalAuthenticationServiceException {
 
         Clients client = clientsRepository.findByUserName(userName);
-        log.info("loadUserByUsername : was called!");
-        log.info("client : " + client);
+
 
         if (client == null){
             log.error("UserUnknown was thrown");
+
             throw new InternalAuthenticationServiceException(this.messages
                     .getMessage("AbstractUserDetailsAuthenticationProvider.UserUnknown",
                             "User was not found."));
         } else {
+
             try {
                 return new User(userName, client.getPassword(), new ArrayList<>());
+
             } catch (InternalAuthenticationServiceException internalAuthenticationServiceException) {
 
             log.error("badCredentials was thrown: " + internalAuthenticationServiceException.getMessage());
@@ -54,41 +60,5 @@ public class ClientsDetailsService implements UserDetailsService {
                             "User found but password is wrong."));
             }
         }
-    }
-
-    @ExceptionHandler(value = {CustomException.class, InternalAuthenticationServiceException.class})
-    public void handleClientsCredentialsError(Exception ex,
-                                              CustomException customException,
-                                              InternalAuthenticationServiceException internalAuthenticationServiceException){
-
-        log.info("Some error was handled: " + ex.getMessage());
-
-        try{
-            if (ex instanceof CustomException){
-                log.error("DisableException was handled: " + customException);
-            }
-            if (ex instanceof InternalAuthenticationServiceException){
-                log.error("InternalAuthenticationServiceException was handled: " + internalAuthenticationServiceException);
-            }
-        } catch (Exception exception) {
-            log.warn("Handling of [" + ex.getClass().getName() + "] resulted in Exception", exception);
-        }
-
-    }
-
-    @Deprecated
-    public Clients readUserByUserName(String userName) throws UsernameNotFoundException {
-        Clients client = clientsRepository.findByUserName(userName);
-
-        log.info("readUserByUserName : was called!");
-        log.info("client : " + client);
-//        log.info("client userName : " + client.getUserName());
-//        log.info("client password : " + client.getPassword());
-
-        if (client == null){
-            throw new UsernameNotFoundException("User not found or does not exist.");
-        }
-
-        return client;
     }
 }
