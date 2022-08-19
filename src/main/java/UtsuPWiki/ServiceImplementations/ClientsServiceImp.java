4b2a109da6c86f5375387cf0d5c9f630d9eb2043
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-19 21:44:41 +0200

    New method implemented

--- src/main/java/UtsuPWiki/ServiceImplementations/ClientsServiceImp.java
+++ src/main/java/UtsuPWiki/ServiceImplementations/ClientsServiceImp.java
@@ -1,6 +1,30 @@
 package UtsuPWiki.ServiceImplementations;
 
+import UtsuPWiki.Entity.Clients;
+import UtsuPWiki.Repository.ClientsRepository;
 import UtsuPWiki.Service.ClientsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+@Log4j2
+@Service
 public class ClientsServiceImp implements ClientsService {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    ClientsRepository clientsRepository;
+
+    @Override
+    public void registerClient(Clients client) {
+        client.setRegistrationDate(new Date().toString());
+        client.setPassword(passwordEncoder.encode(client.getPassword()));
+
+        clientsRepository.save(client);
+        log.info("New user was registered.");
+    }
 }
