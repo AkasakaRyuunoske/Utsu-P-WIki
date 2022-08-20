f5e2b57a3bab9eb73dbc2c8a31da3ab821d41e18
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-20 18:22:08 +0200

    new method added

--- src/main/java/UtsuPWiki/Repository/GenresRepository.java
+++ src/main/java/UtsuPWiki/Repository/GenresRepository.java
@@ -2,8 +2,14 @@ package UtsuPWiki.Repository;
 
 import UtsuPWiki.Entity.Genres;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface GenresRepository extends JpaRepository<Genres, Long> {
+    @Query(value = "SELECT name FROM Genres",
+            nativeQuery = true)
+    ArrayList<String> getAllGenresNames();
 }
