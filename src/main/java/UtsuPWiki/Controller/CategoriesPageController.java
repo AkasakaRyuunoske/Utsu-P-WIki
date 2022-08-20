ea12bda62c0e19d7025c536426d7ffb37a5883d9
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-20 18:21:56 +0200

    controller added

--- src/main/java/UtsuPWiki/Controller/CategoriesPageController.java
+++ src/main/java/UtsuPWiki/Controller/CategoriesPageController.java
@@ -1,4 +1,29 @@
 package UtsuPWiki.Controller;
 
+import UtsuPWiki.Repository.GenresRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+@Log4j2
+@Controller
 public class CategoriesPageController {
+    @Autowired
+    GenresRepository genresRepository;
+
+    @GetMapping("/home/categories")
+    public String GETTypes(Model model,
+                           HttpServletRequest request){
+//        ArrayList<String> genresNames = genresRepository.getAllGenresNames();
+//
+//        model.addAttribute("genresNames", genresNames);
+
+
+        return "categories";
+    }
 }
