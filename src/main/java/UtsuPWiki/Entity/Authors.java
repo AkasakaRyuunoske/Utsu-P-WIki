f1dc35fc5fb9c521daf1373aaeb0f794fdf1c76c
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-21 14:10:43 +0200

    Entities rework

---
+++ src/main/java/UtsuPWiki/Entity/Authors.java
@@ -0,0 +1,45 @@
+package UtsuPWiki.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Authors {
+
+    @Id
+    @SequenceGenerator(name = "Authors_Sequence", sequenceName = "Authors_Sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Authors_Sequence")
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "author_pseudonym", nullable = false)
+    private String author_pseudonym;
+
+    @Column(name = "date_of_birth")
+    private String date_of_birth;
+
+    @Column(name = "total_masterpieces", nullable = false)
+    private int total_masterpieces = 0;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "country_id_fk", referencedColumnName = "id")
+    private Countries countries;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "main_type_id_fk")
+    private Types types;
+}
