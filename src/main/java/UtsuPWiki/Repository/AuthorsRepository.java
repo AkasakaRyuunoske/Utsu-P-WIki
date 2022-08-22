47433df03fe7072ec709c07aa0426bbd7388dca3
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-22 19:54:15 +0200

    New method

--- src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
+++ src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
@@ -12,4 +12,7 @@ public interface AuthorsRepository extends JpaRepository<Authors, Long> {
     @Query("SELECT authorPseudonym FROM Authors")
     String[] getAllAuthorsPseudonyms();
 
+    @Query("SELECT authors.authorPseudonym FROM Authors as authors, Types as types_table where authors.types = types_table.id AND types_table.name = :type")
+    String[] getAllAuthorsPseudonymsByType(String type);
+
 }
