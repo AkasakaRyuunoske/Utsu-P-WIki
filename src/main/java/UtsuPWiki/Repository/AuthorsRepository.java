3946e286709ada77e5fa28001fde5463887cb48e
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-22 16:38:28 +0200

    Entity and repository rework

--- src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
+++ src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
@@ -2,10 +2,14 @@ package UtsuPWiki.Repository;
 
 import UtsuPWiki.Entity.Authors;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorsRepository extends JpaRepository<Authors, Long> {
-    Authors findByAuthor_Pseudonym(String author_pseudonym);
+    @Query("SELECT authorPseudonym FROM Authors")
+    String[] getAllAuthorsPseudonyms();
 
 }
