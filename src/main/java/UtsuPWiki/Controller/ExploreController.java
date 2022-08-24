c41684a498f647bf8460d7f978351858f133cc17
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-24 19:10:36 +0200

    Genres and Types lists added

--- src/main/java/UtsuPWiki/Controller/ExploreController.java
+++ src/main/java/UtsuPWiki/Controller/ExploreController.java
@@ -1,7 +1,10 @@
 package UtsuPWiki.Controller;
 
+import UtsuPWiki.Repository.GenresRepository;
+import UtsuPWiki.Repository.TypesRepository;
 import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,23 @@ import javax.servlet.http.HttpServletRequest;
 @Log4j2
 @Controller
 public class ExploreController {
+    @Autowired
+    TypesRepository typesRepository;
+
+    @Autowired
+    GenresRepository genresRepository;
+
     @GetMapping("/explore")
     public String GETExplore(Model model, HttpServletRequest request){
         Navigation.addLocations(model, request);
+
+        String [] typesList = typesRepository.getAllTypesNames();
+
+        String [] genresList = genresRepository.getAllGenresNames();
+
+        model.addAttribute("typesList", typesList);
+        model.addAttribute("genresList", genresList);
+
 
         return "explore";
     }
