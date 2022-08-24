acc64d8af635948537d7fa7b53e36b04351a1d44
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-24 16:12:49 +0200

    Navigation method added

--- src/main/java/UtsuPWiki/Controller/CustomCMS/CreateController.java
+++ src/main/java/UtsuPWiki/Controller/CustomCMS/CreateController.java
@@ -1,5 +1,6 @@
 package UtsuPWiki.Controller.CustomCMS;
 
+import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 
 /**
  * Will serve creation of the html pages from front-end.
@@ -21,8 +21,7 @@ public class CreateController {
     public String GETCreate(Model model,
                             HttpServletRequest request,
                             HttpServletResponse response){
-        log.info("some one wants to create something!");
-        log.info("request for /create contains: " + Arrays.toString(request.getCookies()));
+        Navigation.addLocations(model, request);
         return "CMS/create";
     }
 
