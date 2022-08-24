51ce48f99d5bb7d34ea34566b08599b1b4eb5132
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-24 19:11:33 +0200

    Documentation added + minor bag fix

--- src/main/resources/static/js/Components/header.js
+++ src/main/resources/static/js/Components/header.js
@@ -1,10 +1,17 @@
+//	header.js is a component responsive for generating headers. 
+//	Other than that the main header with progect name is made dynamic.
+//	plus shows the current logged user (if not, Annonymus is shown) using userName cookie.
 var headers = document.getElementById("headers");
-headers.innerHTML = 
+
+if (headers != null) {
+	headers.innerHTML = 
 			  "<header class='fav-color hero-container'>" 
 			+	"<h1 class='hero glitch layers' data-text='鬱P Wiki' id='Utsu-P'>"
 			+		"鬱P Wiki"
 			+	"</h1>"
 			+ "</header>"
+
+		    + "<h1 th:if='${errorMessage}' class='error header-options-text' th:text='*{errorMessage}'></h1>"
 
 			+	"<header class='header-options-text header-grid-container'>"
 			+		"<h2 class='header-grid-item' onclick='navigate(this)'>Explore</h2>"
@@ -12,13 +19,14 @@ headers.innerHTML =
 			+		"<h2 class='header-grid-item' onclick='navigate(this)'>Create</h2>"
 			+		"<h2 class='header-grid-item' onclick='navigate(this)'>Buy</h2>"
 			+	"</header>";
+}
 
 let utsup = document.getElementById("Utsu-P");
 let counter = 0;
 
 var currentUser = document.getElementById("currentUser");
 
-
+//is implemented as counter because in the future there will be more than 2 variations.
 document.getElementById("Utsu-P").onclick = function () {
 	if (counter == 0) {
 		utsup.dataset.text = "Utsu-P Wiki";
