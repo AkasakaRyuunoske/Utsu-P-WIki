2f2a40188fc65580673c0e4e4dcafbcefee0cfdb
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-24 16:13:15 +0200

    New Component

---
+++ src/main/resources/static/js/Components/footer.js
@@ -0,0 +1,52 @@
+var footer = document.getElementById("footer");
+footer.innerHTML = 	
+		"<footer class='footer'>"
++		"<div style='padding-left:5%;'>"
++		"	<h2>Created By:"
++		"		<span class='fav-color' style='font-family: 'Old English Text MT'>Akasaka Ryuunosuke</span>"
++		"		<p>GitHub:"
++		"			<a href='https://github.com/AkasakaRyuunoske'>https://github.com/AkasakaRyuunoske</a>"
++		"		</p>"
++		"	</h2>"
+
++		"	<br>"
+
++		"	<h2>Special thanks to: 鬱P / Utsu-P</h2>"
+
++		"	<br>"
+
++		"	<h3>His Socials:"
++		"		<p>Youtube: "
++		"			<a href='https://www.youtube.com/c/utsupofficial/'>https://www.youtube.com/c/utsupofficial/</a>"
++		"		</p>"
+
++		"		<p>Official Site:"
++		"			<a href='https://msis.jimdofree.com/'>https://msis.jimdofree.com/</a>"
++		"		</p>"
+
++		"		<p>Apple Music:"
++		"			<a href='https://music.apple.com/jp/artist/鬱p/383043845'>https://music.apple.com/jp/artist/鬱p/383043845</a>"
++		"		</p> "
+
++		"		<p>Twitter:"
++		"			<a href='https://twitter.com/asshole_wii'>https://twitter.com/asshole_wii</a>"
++		"		</p>"
+
++		"		<p>Spotify:"
++		"			<a href='https://open.spotify.com/artist/3Zo5u5IljPxQEfbNY2OgsX?si=KcSszKJqQb6JpRnRiCxKaA&nd=1'>https://open.spotify.com/artist/3Zo5u5IljPxQEfbNY2OgsX?si=KcSszKJqQb6JpRnRiCxKaA&nd=1</a>"
++		"		</p>"
+
++		"		<p>NicoNico:"
++		"			<a href='https://www.nicovideo.jp/user/176618/mylist/6385075'>https://www.nicovideo.jp/user/176618/mylist/6385075</a>"
++		"		</p> "
+
++		"		<p>Instagram:"
++		"			<a href='https://www.instagram.com/asshole_wii/'>https://www.instagram.com/asshole_wii/user/176618/mylist/6385075</a>"
++		"		</p>"
+
++		"		<p>linktr:"
++		"			<a href='https://linktr.ee/asshole_wii'>linktr.ee/asshole_wii</a>"
++		"		</p>"
++		"	</h3>"
++	"	</div>"
++	"</footer>"
