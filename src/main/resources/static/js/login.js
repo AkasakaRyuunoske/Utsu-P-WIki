97d495c620844ae280154cd898390198e5b4293c
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-24 19:12:16 +0200

    documentation added

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,7 @@
+//	login.js is responsive of loging-in the user.
+//	takes the data and makes an AJAX(Jquery) Call with all the data
+//	but preventing the default behavior of the htmlForm.
+//	If credentials are wrong, so AJAX call error case is called
 var form  = document.getElementById('myForm');
 
 var userName;
@@ -6,7 +10,9 @@ var password;
 var error = document.getElementById('userNameOrPasswordError');
 
 form.onsubmit = function(event){
-	event.preventDefault(); // prrevents default form submitting.
+	// prrevents default form submitting. If not prevented 
+	//after submit user will be on ../login? url
+	event.preventDefault(); 
 
 	userName = document.getElementById('userName').value;
 	password = document.getElementById('password').value;
@@ -17,25 +23,23 @@ form.onsubmit = function(event){
 
 	var json = JSON.stringify(Object.fromEntries(formData));
 
-$.ajax({
-	  type: "POST",
-	  url:PREFIX_LOCAL + "login",
-	  async: false,
-	  data: json,
-	  contentType : "application/json",
+	$.ajax({
+		type: "POST",
+		url:PREFIX_LOCAL + "login",
+		async: false,
+		data: json,
+		contentType : "application/json",
 
-	  success: function(data, textStatus, request){
+		success: function(data, textStatus, request){
 		window.location.href = "/";
-	  },
+		},
 
-	  //In order to be able to read response header
-	  //function must follow this temoplate.
-	  //the function must be absolutly same as below 
-	  //otherwise it DOES NOT WORK.
-	  error : function(request, textStatus, errorThrown) {
+		//In order to be able to read response header
+		//function must follow this temoplate.
+		//the function must be absolutly same as below 
+		//otherwise it DOES NOT WORK.
+		error : function(request, textStatus, errorThrown) {
 			error.innerHTML = request.getResponseHeader('errorMessage');
-	  }
+		}
 	});
 }
-
-//.failureHandler((request, response, exception) -> System.out.println(exception))
