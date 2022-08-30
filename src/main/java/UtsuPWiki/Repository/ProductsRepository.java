f09cef727e3f2ea47b4f7f174aa452e473b7cd8c
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-08-30 20:00:13 +0200

    GetAllProductNames added to future use

--- src/main/java/UtsuPWiki/Repository/ProductsRepository.java
+++ src/main/java/UtsuPWiki/Repository/ProductsRepository.java
@@ -2,8 +2,14 @@ package UtsuPWiki.Repository;
 
 import UtsuPWiki.Entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
+    @Query("SELECT productName FROM Products")
+    String[] getAllProductsNames();
+
+//    @Query("SELECT * FROM Products where author_id_fk = :author_id")
+//    String[] getAllproductsByAuthorsId();
 }
