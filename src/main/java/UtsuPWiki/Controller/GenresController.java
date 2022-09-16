10c8010eb5e54722b2700c98f62312be3bce324d
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-09-16 11:56:36 +0200

    New controller to access precise genre added

--- src/main/java/UtsuPWiki/Controller/GenresController.java
+++ src/main/java/UtsuPWiki/Controller/GenresController.java
@@ -1,11 +1,14 @@
 package UtsuPWiki.Controller;
 
+import UtsuPWiki.Repository.AuthorsRepository;
 import UtsuPWiki.Repository.GenresRepository;
+import UtsuPWiki.utilities.Navigation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,14 +18,33 @@ public class GenresController {
     @Autowired
     GenresRepository genresRepository;
 
-    @GetMapping("/home/categories")
-    public String GETTypes(Model model,
+    @Autowired
+    AuthorsRepository authorsRepository;
+
+    @GetMapping("/genres")
+    public String GETGenres(Model model,
                            HttpServletRequest request){
-//        ArrayList<String> genresNames = genresRepository.getAllGenresNames();
-//
-//        model.addAttribute("genresNames", genresNames);
+        Navigation.addLocations(model, request);
 
+        String [] genresList = genresRepository.getAllGenresNames();
+        model.addAttribute("genresList", genresList);
 
-        return "categories";
+        return "genres";
+    }
+
+    @GetMapping("/genres/{genre}")
+    public String GETGenre(Model model,
+                           HttpServletRequest request,
+                           @PathVariable String genre){
+        Navigation.addLocations(model, request);
+
+        String [] genresList = genresRepository.getAllGenresNames();
+        model.addAttribute("genresList", genresList);
+
+        String [] authorsByGenreList = authorsRepository.getAllAuthorsPseudonymsByGenre(genre);
+        model.addAttribute("authorsByGenreList", authorsByGenreList);
+
+        log.info("Genres are: " + genre);
+        return "Genres/" + genre;
     }
 }
