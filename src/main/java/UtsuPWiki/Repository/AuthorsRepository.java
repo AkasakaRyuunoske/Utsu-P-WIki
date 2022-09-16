05d180fc330ca70adc91ffbef1ef23e7ad3f1610
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-09-16 11:55:40 +0200

    New method added to retrieve authors by genre

--- src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
+++ src/main/java/UtsuPWiki/Repository/AuthorsRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AuthorsRepository extends JpaRepository<Authors, Long> {
     @Query("SELECT authorPseudonym FROM Authors")
@@ -15,4 +13,6 @@ public interface AuthorsRepository extends JpaRepository<Authors, Long> {
     @Query("SELECT authors.authorPseudonym FROM Authors as authors, Types as types_table where authors.types = types_table.id AND types_table.name = :type")
     String[] getAllAuthorsPseudonymsByType(String type);
 
+    @Query("SELECT authors.authorPseudonym FROM Authors as authors, Genres as genres_table where authors.genres = genres_table.id AND genres_table.name = :genre")
+    String[] getAllAuthorsPseudonymsByGenre(String genre);
 }
