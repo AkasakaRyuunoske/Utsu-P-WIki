b4e4e026c7366746730a7c95031651bec01b836a
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-12 21:00:18 +0200

    new class added: Fancy writings to manage on startup writings

---
+++ src/main/java/UtsuPWiki/utilities/FancyWritings.java
@@ -0,0 +1,22 @@
+package UtsuPWiki.utilities;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class FancyWritings {
+
+    public void printMotivationalThingOnStartUp(boolean doPrint) {
+        if (doPrint) {
+            log.info("\n" + "\n" +
+                    "\"Воля может и должна быть предметом гордости гораздо больше, нежели талант.\n" +
+                    " Если талант — это развитие природных склонностей, \n" +
+                    " то твёрдая воля — это ежеминутно одерживаемая победа над инстинктами,\n" +
+                    " над влечениями, которые воля обуздывает и подавляет, над препятствиями\n" +
+                    " и преградами, которые она осиливает, над всяческими трудностями,\n" +
+                    " которые она героически преодолевает. \" (Оноре де Бальзак)\""
+                    + "\n");
+        }
+    }
+}
