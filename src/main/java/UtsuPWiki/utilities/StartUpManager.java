307388b8a3fcc20187809d14c25fce036da145df
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-14 19:54:05 +0200

    Auto database populating: Configurations are added to StartUpManager.java

--- src/main/java/UtsuPWiki/utilities/StartUpManager.java
+++ src/main/java/UtsuPWiki/utilities/StartUpManager.java
@@ -13,6 +13,35 @@ public class StartUpManager {
     @Value("${database-helper.populate-database}")
     private boolean doPopulateDatabase;
 
+    @Value("${database-helper.populate-authors}")
+    private boolean doPopulateAuthors;
+
+    @Value("${database-helper.populate-clients}")
+    private boolean doPopulateClients;
+
+    @Value("${database-helper.populate-comments}")
+    private boolean doPopulateComments;
+
+    @Value("${database-helper.populate-countries}")
+    private boolean doPopulateCountries;
+
+    @Value("${database-helper.populate-delivery}")
+    private boolean doPopulateDelivery;
+
+    @Value("${database-helper.populate-genres}")
+    private boolean doPopulateGenres;
+
+    @Value("${database-helper.populate-orders}")
+    private boolean doPopulateOrders;
+
+    @Value("${database-helper.populate-products}")
+    private boolean doPopulateProducts;
+    @Value("${database-helper.populate-types}")
+    private boolean doPopulateTypes;
+
+    @Value("${database-helper.populate-all}")
+    private boolean doPopulateAll;
+
     @Value("${fancy-writings.do-print}")
     private boolean doPrint;
 
@@ -25,7 +54,18 @@ public class StartUpManager {
     @EventListener(ApplicationReadyEvent.class)
     public void manageStartUp(){
         fancyWritings.printMotivationalThingOnStartUp(doPrint);
-        databaseHelper.doSomethingAfterStartup(doPopulateDatabase);
+        databaseHelper.populateDatabase(
+                doPopulateDatabase,
+                doPopulateAuthors,
+                doPopulateClients,
+                doPopulateComments,
+                doPopulateCountries,
+                doPopulateDelivery,
+                doPopulateGenres,
+                doPopulateOrders,
+                doPopulateProducts,
+                doPopulateTypes,
+                doPopulateAll);
 
     }
 }
