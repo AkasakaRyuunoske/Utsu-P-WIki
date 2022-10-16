a27368fd767603226a67683f22c8c5a29bd93fa3
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-16 16:05:08 +0200

    Application listener added

--- src/main/java/UtsuPWiki/UtsuPApplication.java
+++ src/main/java/UtsuPWiki/UtsuPApplication.java
@@ -1,13 +1,18 @@
 package UtsuPWiki;
 
+import UtsuPWiki.utilities.StartUpManager;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @Log4j2
 @SpringBootApplication
 public class UtsuPApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(UtsuPApplication.class, args);
+//		SpringApplication.run(UtsuPApplication.class, args);
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(UtsuPApplication.class)
+				.listeners(new StartUpManager()).run();
 	}
 }
