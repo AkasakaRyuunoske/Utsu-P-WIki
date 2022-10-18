3b4cefff77f981736f370e0447ebc0f59b3d6354
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-18 21:08:28 +0200

    return to ApplicationEnvironmentPreparedEvent based configuration of the class

--- src/main/java/UtsuPWiki/utilities/StartUpManager.java
+++ src/main/java/UtsuPWiki/utilities/StartUpManager.java
@@ -2,47 +2,34 @@ package UtsuPWiki.utilities;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 
 @Log4j2
-public class StartUpManager implements ApplicationListener<ApplicationStartedEvent> {
+public class StartUpManager implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
     FancyWritings fancyWritings = new FancyWritings();
     DatabaseHelper databaseHelper = new DatabaseHelper();
 
     @Override
-    public void onApplicationEvent(ApplicationStartedEvent event) {
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
 
         /* manually getting all the properties from application.properties because
          At ApplicationEnvironmentPreparedEvent annotations do not work yet */
 
         // database-helper properties
-//        String doPopulateDatabase   = event.getEnvironment().getProperty("database-helper.populate-database" );
-//        String doPopulateAuthors    = event.getEnvironment().getProperty("database-helper.populate-authors"  );
-//        String doPopulateClients    = event.getEnvironment().getProperty("database-helper.populate-clients"  );
-//        String doPopulateComments   = event.getEnvironment().getProperty("database-helper.populate-comments" );
-//        String doPopulateCountries  = event.getEnvironment().getProperty("database-helper.populate-countries");
-//        String doPopulateDelivery   = event.getEnvironment().getProperty("database-helper.populate-delivery" );
-//        String doPopulateGenres     = event.getEnvironment().getProperty("database-helper.populate-genres"   );
-//        String doPopulateOrders     = event.getEnvironment().getProperty("database-helper.populate-orders"   );
-//        String doPopulateProducts   = event.getEnvironment().getProperty("database-helper.populate-products" );
-//        String doPopulateTypes      = event.getEnvironment().getProperty("database-helper.populate-types"    );
-//        String doPopulateAll        = event.getEnvironment().getProperty("database-helper.populate-all"      );
-
-        String doPopulateDatabase   = "true";
-        String doPopulateAuthors    = "false";
-        String doPopulateClients    = "false";
-        String doPopulateComments   = "false";
-        String doPopulateCountries  = "false";
-        String doPopulateDelivery   = "false";
-        String doPopulateGenres     = "false";
-        String doPopulateOrders     = "false";
-        String doPopulateProducts   = "false";
-        String doPopulateTypes      = "false";
-        String doPopulateAll        = "true";
+        String doPopulateDatabase   = event.getEnvironment().getProperty("database-helper.populate-database" );
+        String doPopulateAuthors    = event.getEnvironment().getProperty("database-helper.populate-authors"  );
+        String doPopulateClients    = event.getEnvironment().getProperty("database-helper.populate-clients"  );
+        String doPopulateComments   = event.getEnvironment().getProperty("database-helper.populate-comments" );
+        String doPopulateCountries  = event.getEnvironment().getProperty("database-helper.populate-countries");
+        String doPopulateDelivery   = event.getEnvironment().getProperty("database-helper.populate-delivery" );
+        String doPopulateGenres     = event.getEnvironment().getProperty("database-helper.populate-genres"   );
+        String doPopulateOrders     = event.getEnvironment().getProperty("database-helper.populate-orders"   );
+        String doPopulateProducts   = event.getEnvironment().getProperty("database-helper.populate-products" );
+        String doPopulateTypes      = event.getEnvironment().getProperty("database-helper.populate-types"    );
+        String doPopulateAll        = event.getEnvironment().getProperty("database-helper.populate-all"      );
 
         // fancy writings properties
-        String doPrint              = "true";
+        String doPrint              = event.getEnvironment().getProperty("fancy-writings.do-print"      );
 
         // managing startup
         manageStartUp(doPrint);
