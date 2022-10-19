c2adf2a57af152dc2bf787b99edf0e8f5e8700e4
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-19 20:32:28 +0200

    Documentation added

--- src/main/java/UtsuPWiki/utilities/FancyWritings.java
+++ src/main/java/UtsuPWiki/utilities/FancyWritings.java
@@ -1,14 +1,19 @@
 package UtsuPWiki.utilities;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
 
+/**
+ * Prints some quotes (motivational or just useless).
+ * To allow to print and decide what is defined in application.properties.
+ *
+ * Must not be allowed to print in Production build.
+ * */
 @Log4j2
-//@Component
 public class FancyWritings {
 
     public void printMotivationalThingOnStartUp(String doPrint) {
         log.info("is allowed to print? " + doPrint);
+
         if (doPrint.equals("true")) {
             log.info("\n" + "\n" +
                     "\"Воля может и должна быть предметом гордости гораздо больше, нежели талант.\n" +
