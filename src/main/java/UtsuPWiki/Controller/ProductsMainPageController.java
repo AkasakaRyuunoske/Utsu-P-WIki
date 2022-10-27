849356a464c0c4e06d36cd20264d3624acbd8191
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-10-27 10:28:14 +0200

    swap of functionalities

--- src/main/java/UtsuPWiki/Controller/ProductsMainPageController.java
+++ src/main/java/UtsuPWiki/Controller/ProductsMainPageController.java
@@ -1,4 +1,31 @@
 package UtsuPWiki.Controller;
 
+import UtsuPWiki.Entity.Products;
+import UtsuPWiki.Repository.ProductsRepository;
+import UtsuPWiki.utilities.Navigation;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Log4j2
+@Controller
 public class ProductsMainPageController {
+    @Autowired
+    ProductsRepository productsRepository;
+
+    @GetMapping("/buy")
+    public String GETBuy(Model model, HttpServletRequest request){
+        Navigation.addLocations(model, request);
+
+        List<Products> productsList = productsRepository.findAll();
+
+        model.addAttribute("productsList", productsList);
+
+        return "buy";
+    }
 }
