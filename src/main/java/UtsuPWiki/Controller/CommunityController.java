15103f55bbd276f9309ca08ada48e97e14e5a630
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2022-11-02 20:33:23 +0100

    New Controller added

---
+++ src/main/java/UtsuPWiki/Controller/CommunityController.java
@@ -0,0 +1,21 @@
+package UtsuPWiki.Controller;
+
+import UtsuPWiki.utilities.Navigation;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Log4j2
+@Controller
+public class CommunityController {
+
+    @GetMapping("/community")
+    public String GETCommunity(Model model, HttpServletRequest request){
+        Navigation.addLocations(model, request);
+
+        return "community";
+    }
+}
