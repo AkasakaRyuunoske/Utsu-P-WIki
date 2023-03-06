35930a9767223a6de39f74779dcbf62feffacbb6
Akasaka <akasakaryuunoske1@gmail.com>
2023-03-06 18:57:53 +0100

    sub header added

--- src/main/resources/static/js/Components/header.js
+++ src/main/resources/static/js/Components/header.js
@@ -9,8 +9,9 @@ if (headers != null) {
 			+	"<h1 class='hero glitch layers' data-text='鬱P Wiki' id='Utsu-P'>"
 			+		"鬱P Wiki"
 			+	"</h1>"
+			+ "<h2 class='sub-header-text'>Home</h2>"
 			+ "</header>"
-			
+
 			+	"<header class='header-options-text header-grid-container'>"
 			+		"<h2 class='header-grid-item' onclick='navigate(this)'>Explore</h2>"
 			+		"<h2 class='header-grid-item' onclick='navigate(this)'>Shit</h2>"
@@ -23,19 +24,6 @@ let utsup = document.getElementById("Utsu-P");
 let counter = 0;
 
 var currentUser = document.getElementById("currentUser");
-
-//is implemented as counter because in the future there will be more than 2 variations.
-// document.getElementById("Utsu-P").onclick = function () {
-// 	if (counter == 0) {
-// 		utsup.dataset.text = "Utsu-P Wiki";
-// 		utsup.innerHTML = "Utsu-P Wiki";
-// 		counter = 1;
-// 	} else {
-// 		utsup.dataset.text = "鬱P Wiki";
-// 		utsup.innerHTML = "鬱P Wiki";
-// 		counter = 0;
-// 	}
-// }
 
 document.getElementById("Utsu-P").onclick = function () {
 	switch(counter){
@@ -92,7 +80,7 @@ function getCookie(cookie_name) {
   let decodedCookie = decodeURIComponent(document.cookie);
   let cookies = decodedCookie.split(';');
 
-  for(let i = 0; i <cookies.length; i++) {
+  for(let i = 0; i < cookies.length; i++) {
     let cookie = cookies[i];
     while (cookie.charAt(0) == ' ') {
       cookie = cookie.substring(1);
