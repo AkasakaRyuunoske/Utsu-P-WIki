d26fd7e6bdb01ecce6456a4256d67caf2d1f3530
Akasaka <akasakaryuunoske1@gmail.com>
2023-03-08 15:26:49 +0100

    functionality added

--- src/main/resources/static/js/Components/navigation-component.js
+++ src/main/resources/static/js/Components/navigation-component.js
@@ -1,7 +1,26 @@
-let location = document.getElementById('location').innerHTML.toLowerCase();
 let locationsContainer = document.getElementById('locationsContainer').textContent;
+let currentPosition;
 
-locationsContainer = locationsContainer.replaceAll("\n", "");
-locationsContainer = locationsContainer.replaceAll("\t", "");
+if (locationsContainer != null) {
 
-let locationIndex = locationsContainer.indexOf(location);
+	locationsContainer = locationsContainer.replaceAll("\n", "");
+	locationsContainer = locationsContainer.replaceAll("\t", "");
+
+	currentPosition = locationsContainer.split("/");
+
+	// https://stackoverflow.com/questions/3216013/get-the-last-item-in-an-array
+	// According to the top upvoted answer ES-2022 allows you to do shit below
+	// --->  currentPosition = currentPosition.at(-1);
+	// But we not gonna do this, instead:
+	currentPosition = currentPosition[currentPosition.length-1]
+} else {
+	console.log("Something went wrong.");
+}
+
+subHeader = document.getElementById("sub-header");
+
+if (subHeader != null) {
+	subHeader.innerHTML = currentPosition;
+} else {
+	console.log("Something went wrong.");
+}
