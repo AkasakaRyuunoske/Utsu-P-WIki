529e65f7d0d0691b2544f8c1b9896bd1270fe250
Akasaka <akasakaryuunoske1@gmail.com>
2023-03-08 20:46:43 +0100

    2 new functions added to manage products

--- src/main/java/UtsuPWiki/Repository/ProductsRepository.java
+++ src/main/java/UtsuPWiki/Repository/ProductsRepository.java
@@ -1,6 +1,7 @@
 package UtsuPWiki.Repository;
 
 import UtsuPWiki.Entity.Products;
+import lombok.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,15 @@ public interface ProductsRepository extends JpaRepository<Products, Long> {
     String[] getAllProperties();
 //    @Query("SELECT * FROM Products where author_id_fk = :author_id")
 //    String[] getAllproductsByAuthorsId();
+    @Query(value = "SELECT * FROM Products WHERE product_name = :name", nativeQuery = true)
+    Products selectProductByProductName(String name);
+
+    //todo get track list by name
+
+    @Query(value = "SELECT products_additional_info.song_name " +
+                   "FROM products " +
+                   "JOIN products_additional_info ON products.id = products_additional_info.id " +
+                   "WHERE products_additional_info.product_id_fk = :id_album",
+                   nativeQuery = true)
+    String[] selectTrackListByAlbumId(long id_album);
 }
