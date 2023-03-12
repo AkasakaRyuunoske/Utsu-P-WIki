2ccab9881867918d2e03f7fc3854a875c459c8e0
Akasaka <akasakaryuunoske1@gmail.com>
2023-03-12 11:16:17 +0100

    contains function added, useless code deleted

--- src/main/java/UtsuPWiki/Repository/ProductsRepository.java
+++ src/main/java/UtsuPWiki/Repository/ProductsRepository.java
@@ -10,19 +10,13 @@ import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
-    @Query("SELECT productName FROM Products")
-    String[] getAllProductsNames();
-
-    @Query(value = "SELECT additional_info FROM Products", nativeQuery = true)
-    String[] getAllProperties();
-//    @Query("SELECT * FROM Products where author_id_fk = :author_id")
-//    String[] getAllproductsByAuthorsId();
     @Query(value = "SELECT * FROM Products WHERE product_name = :name", nativeQuery = true)
     Products selectProductByProductName(String name);
 
-    //todo get track list by name
-
-    @Query(value = "SELECT song_name, id FROM products_additional_info WHERE product_id_fk = :id_album",
+    @Query(value = "SELECT id, song_name FROM products_additional_info WHERE product_id_fk = :id_album",
                    nativeQuery = true)
     String[] selectTrackListByAlbumId(long id_album);
+
+    @Query(value = "select contains from products where id = :id", nativeQuery = true)
+    String selectContainsById(long id);
 }
