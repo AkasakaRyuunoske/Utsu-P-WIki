66c44c315e202e9795ff961a776aafef1c4d3912
Akasaka <akasakaryuunoske1@gmail.com>
2023-03-17 11:26:10 +0100

    SetContains method added

--- src/main/resources/static/js/buy.js
+++ src/main/resources/static/js/buy.js
@@ -10,18 +10,35 @@ function buy(element){
 
 function setTrackList(){
 	let trackList = document.getElementsByClassName("contains");
-	let collectionArr = [].slice.call(trackList);
 
-	for(let i = 0; i < trackList.length ; i++){
-	    let result = collectionArr[i].innerHTML.replace(","," ");
-	    document.getElementsByClassName("contains")[i].innerHTML = i + " " + result;
+	if (trackList.length > 0) {
+		let collectionArr = [].slice.call(trackList);
 
+		for(let i = 0; i < trackList.length ; i++){
+		    let result = collectionArr[i].innerHTML.replace(","," ");
+		    document.getElementsByClassName("contains")[i].innerHTML = i + " " + result;
+
+		}
 	}
+
 }
 
 setTrackList();
 
 function setContains(){
-	
+	//document.getElementsByTagName("li")[0].innerText = document.getElelementsByTagName("li")[0].innerText.replaceAll(",","\n");
+	let contains = document.getElementsByTagName("li");
+
+	if (contains.length > 0) {
+		let collectionArr = [].slice.call(contains);
+
+		for(let i = 0; i < contains.length ; i++){
+		    let result = collectionArr[i].innerText.replaceAll(",","\n");
+		    document.getElementsByTagName("li")[i].innerText = result;
+
+		}
+	}
 }
+
+setContains();
 
